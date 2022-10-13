567189cbf8f2b2feb3e66015242bbc48825c6c4a
Anirudha Gupta <anirudhagupta01@gmail.com>
2022-10-13 08:20:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 121;
+            IsPalindrome(x);
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+            int palindrome = x;
+            int reverse = 0;
+            while (palindrome != 0)
+            {
+                int remainder = palindrome % 10;
+                reverse = reverse * 10 + remainder;
+                palindrome = palindrome / 10;
+            }
+            if (x == reverse)
+                return true;
+            return false;
+        }
+    }
+}
